df6f0bc439e98b772d2680fe31a45d69d60dc9da
Juan Pablo Aranda Galvis <juan.aranda.galvis@gmail.com>
2022-08-27 23:21:55 -0500

    Added log to measure time to containers start

--- testcontainers-mockserver/src/test/java/org/juan/learn/testcontainers/Containers.java
+++ testcontainers-mockserver/src/test/java/org/juan/learn/testcontainers/Containers.java
@@ -1,18 +1,27 @@
 package org.juan.learn.testcontainers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MockServerContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.Stream;
 
+
+@Slf4j
 public class Containers {
 
     public static final MySQLContainer MYSQL;
     public static final MockServerContainer EMAIL_SERVICE;
 
     static {
+        Instant start = Instant.now();
+
         MYSQL = new MySQLContainer(DockerImageName.parse("mysql:5.7"));
         MYSQL.withDatabaseName("customer_db");
         MYSQL.withUsername("user");
@@ -23,6 +32,9 @@ public class Containers {
 
         EMAIL_SERVICE = new MockServerContainer(DockerImageName.parse("mockserver/mockserver:5.13.2"));
         EMAIL_SERVICE.start();
+
+        //Stream.of(MYSQL, EMAIL_SERVICE).parallel().forEach(GenericContainer::start);
+        log.info("🐳 TestContainers started in {}", Duration.between(start, Instant.now()));
 
     }
 
